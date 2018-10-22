148ca568b27ada66f34ae95115a3b1b409fddfbe
Bibek Bhattarai <bhattarai.beck@gmail.com>
2018-10-22 15:12:43 -0400

    Delete fuseRelaunch.cmd

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/TOSHIBA/Desktop/Cordic_processor_Qvalue_Ivalue/Cordic_isim_beh.exe" -prj "C:/Users/TOSHIBA/Desktop/Cordic_processor_Qvalue_Ivalue/Cordic_beh.prj" "work.Cordic" "work.glbl" 
